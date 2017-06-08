70182e377de3e1540f902ac2b23636e1d80e9b14
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-06-08 10:17:34 -0400

    add presidents array

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var presidents = [
+    {
+        firstName: 'Barack',
+        lastName: 'Obama',
+        termLength: 2,
+        party: 'Democrat',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'George W.',
+        lastName: 'Bush',
+        termLength: 2,
+        party: 'Republican',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'Bill',
+        lastName: 'Clinton',
+        termLength: 2,
+        party: 'Democrat',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'George',
+        lastName: 'Bush',
+        termLength: 1,
+        party: 'Republican',
+        yearsOfPresidency: 4
+    },
+
+    {
+        firstName: 'Ronald',
+        lastName: 'Reagan',
+        termLength: 2,
+        party: 'Democrat',
+        yearsOfPresidency: 8
+    }
+];
+
+console.log(presidents[1].firstName);
+
+console.log(presidents[1].lastName);
